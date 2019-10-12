0127851cae778ce7642ee6a2af91088d9feb5f16
Kitsune IDK <velocicraft2088@gmail.com>
2019-10-12 17:38:33 -0300

    Official release!

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # Soft Dynamics - DynaMathLib for Java
 
+## Shields
+
+Builds:
 [![](https://jitpack.io/v/tk.softdynamics/DynaMathLib.svg)](https://jitpack.io/#tk.softdynamics/DynaMathLib)
 [![Build Status](https://travis-ci.org/soft-dynamics/DynaMathLib.svg?branch=master)](https://travis-ci.org/soft-dynamics/DynaMathLib)
+
+Versions:
 ![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/soft-dynamics/DynaMathLib?label=GitHub%20Release&sort=semver)
+![JitPack](https://img.shields.io/jitpack/v/github/soft-dynamics/DynaMathLib)
+
+GitHub:
 ![GitHub pull requests](https://img.shields.io/github/issues-pr/soft-dynamics/DynaMathLib)
 ![GitHub issues](https://img.shields.io/github/issues/soft-dynamics/DynaMathLib)
-![GitHub](https://img.shields.io/github/license/soft-dynamics/DynaMathLib)
 ![GitHub forks](https://img.shields.io/github/forks/soft-dynamics/DynaMathLib)
 ![GitHub followers](https://img.shields.io/github/followers/soft-dynamics)
 ![GitHub stars](https://img.shields.io/github/stars/soft-dynamics/DynaMathLib)
 ![GitHub watchers](https://img.shields.io/github/watchers/soft-dynamics/DynaMathLib)
+
+About:
 ![GitHub commits since latest release](https://img.shields.io/github/commits-since/soft-dynamics/DynaMathLib/latest)
-![JitPack](https://img.shields.io/jitpack/v/github/soft-dynamics/DynaMathLib)
+![GitHub](https://img.shields.io/github/license/soft-dynamics/DynaMathLib)
 
 ## Docs
 
--- src/tk/softdynamics/DynaMath.java
+++ src/tk/softdynamics/DynaMath.java
@@ -2,13 +2,51 @@ package tk.softdynamics;
 
 public class DynaMath {
 	
+	//Tales calc
+	public static double tales(int equivalenceA, int equivalenceB, int a) {
+		return (equivalenceB * a) / equivalenceA;
+	}
+	
+	//Pitagoras calc
+	public static double pitagoras(double collaredA, double collaredB) {
+		return Math.sqrt((collaredA * collaredA) + (collaredB * collaredB));
+	}
+	
+	//Perimeter calc
+	public static double perimeter(double width, double heigth) {
+		return (width * 2) + (heigth * 2);
+	}
+	
+	public static double perimeter(double collaredA, double collaredB, double hypotenuse) {
+		return collaredA + collaredB + hypotenuse;
+	}
+	
+	public static double perimeter(double width, int shapes) {
+		return width * shapes;
+	}
+	
+	//Area calc
+	public static double area(double width, double heigth) {
+		return width * heigth;
+	}
+	
+	//Circumference calc
+	public static double circumference(double diameter) {
+		return Math.PI * diameter;
+	}
+	
+	//Circle area calc
+	public static double circleArea(double raio) {
+		return (raio * raio) * Math.PI;
+	}
+	
 	//Delta calc
-	public static double delta(double a, double b, double c) {
+	public static double delta(int a, int b, int c) {
 		return (b * b) - (4 * a * c);
 	}
 	
 	//Bhaskara calc
-	public static String[] bhaskara(double a, double b, double c) throws ArithmeticException {
+	public static String[] bhaskara(int a, int b, int c) throws ArithmeticException {
 		double lower = 2 * a;
 		double upLess = (b * -1) - Math.sqrt(DynaMath.delta(a, b, c));
 		double upMore = (b * -1) + Math.sqrt(DynaMath.delta(a, b, c));
@@ -22,17 +60,18 @@ public class DynaMath {
 		}
 	}
 	
-	public static String[] biquadrado(double a, double b, double c) throws ArithmeticException {
+	//Biquadrado calc
+	public static String[] biquadrado(int a, int b, int c) throws ArithmeticException {
 		if (DynaMath.delta(a, b, c) <= 0) throw new ArithmeticException("Bhaskara result is a fraction or don't have result.");
 		double x12 = Math.sqrt(Double.parseDouble(DynaMath.bhaskara(a, b, c)[0]));
 		double x11 = Math.sqrt(Double.parseDouble(DynaMath.bhaskara(a, b, c)[0])) * -1;
 		double x22 = Math.sqrt(Double.parseDouble(DynaMath.bhaskara(a, b, c)[1]));
 		double x21 = Math.sqrt(Double.parseDouble(DynaMath.bhaskara(a, b, c)[1])) * -1;
-		if (Double.parseDouble(DynaMath.bhaskara(a, b, c)[0]) < 0) {
+		if (Double.parseDouble(DynaMath.bhaskara(a, b, c)[0]) < 0 || Double.parseDouble(DynaMath.bhaskara(a, b, c)[0]) != Math.floor(Double.parseDouble(DynaMath.bhaskara(a, b, c)[0]))) {
 			x12 = 0;
 			x11 = 0;
 		}
-		if (Double.parseDouble(DynaMath.bhaskara(a, b, c)[1]) < 0) {
+		if (Double.parseDouble(DynaMath.bhaskara(a, b, c)[1]) < 0 || Double.parseDouble(DynaMath.bhaskara(a, b, c)[1]) != Math.floor(Double.parseDouble(DynaMath.bhaskara(a, b, c)[1]))) {
 			x22 = 0;
 			x21 = 0;
 		}
@@ -47,19 +86,14 @@ public class DynaMath {
 		}
 	}
 	
-	//Faction boolean
-	private static boolean isFaction(String a) {
-		return a.split("/").length == 2;
-	}
-	
 	//Test handler
 	public static void main(String[] args) throws ArithmeticException, RuntimeException, NumberFormatException {
 		if (args.length != 3) {
 			throw new RuntimeException("Test failed.");
 		}
-		double a = Double.parseDouble(args[0]);
-		double b = Double.parseDouble(args[1]);
-		double c = Double.parseDouble(args[2]);
-		System.out.println(DynaMath.biquadrado(a, b, c)[0]+" "+DynaMath.biquadrado(a, b, c)[1]+" "+DynaMath.biquadrado(a, b, c)[2]+" "+DynaMath.biquadrado(a, b, c)[3]);
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		System.out.println(DynaMath.bhaskara(a, b, c)[0]+" "+DynaMath.bhaskara(a, b, c)[1]);
 	}
 }
